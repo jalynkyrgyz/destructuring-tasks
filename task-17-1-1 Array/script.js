8acbaf238849ba6818b2c436973dcba9fcf1b871
Jalyn Kyrgyz <87895714+jalynkyrgyz@users.noreply.github.com>
2022-03-31 23:11:50 +0600

    Add files via upload

---
+++ task-17-1-1 Array/script.js
@@ -0,0 +1,7 @@
+let arr = ["Иван", "Иванов", "Отдел разработки", "Программист", 2000];
+let [firstName, lastName, department, position, salary] = arr;
+console.log(firstName);
+console.log(lastName);
+console.log(department);
+console.log(position);
+console.log(salary);
